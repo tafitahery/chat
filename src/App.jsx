bf90b105fa31eaa2d03d20d8a273a908f4414f3a
Tafita Hery <tafitahery@gmail.com>
2023-09-29 16:51:25 +0300

    React Login and Register Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Home from './pages/Home';
+import Login from './pages/Login';
 import Register from './pages/Register';
+import './style.scss';
 
 function App() {
   return (
     <div>
-      <Register />
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Add from '../img/addAvatar.png';
+
+export default function Login() {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lebo Chat</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
+}
